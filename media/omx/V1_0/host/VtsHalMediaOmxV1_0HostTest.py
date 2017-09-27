fd535a798b18c58e598daf3c47658ecdf9a9e9de
Zhuoyao Zhang <zhuoyao@google.com>
2017-09-27 09:51:41 -0700

    Run 64bit Omx test with vts.
    Bug: 63796949
    Test: make vts.
          vts-tradefed run vts -m VtsHalMediaOmxV1_0Host
    
    Merged-In: Icf06a2fa2a362ff5901e25590b78f0262f451d1c
    Change-Id: Icf06a2fa2a362ff5901e25590b78f0262f451d1c
    (cherry picked from commit 9a99cb2519ca576a432e43b4d22c250c901c56cd)

--- media/omx/V1_0/host/VtsHalMediaOmxV1_0HostTest.py
+++ media/omx/V1_0/host/VtsHalMediaOmxV1_0HostTest.py
@@ -121,7 +121,7 @@ class VtsHalMediaOmxV1_0Host(hal_hidl_gtest.HidlHalGTest):
                     instance_name = "default"
                     test_case = omx_test_case.VtsHalMediaOmxV1_0TestCase(
                         component, role, instance_name, test_suite, test_name,
-                        path)
+                        path, tag)
                     test_cases.append(test_case)
         logging.info("num of test_testcases: %s", len(test_cases))
         return test_cases
