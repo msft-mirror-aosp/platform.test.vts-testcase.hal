69ffe70eec67adc506bdb651090282935ab06ebd
Roshan Pius <rpius@google.com>
2018-07-25 12:28:16 -0700

    vts(wifi): Separate P2P specific tests
    Similar to how NAN specific tests were separated from wifi HAL VTS
    test suite, separate out P2P specific tests in wpa_supplicant HAL VTS test
    suite.
    Changes in the CL:
    a) Make supplicant@1.0 & supplicant@1.1 tests host side tests so that
    they can check whether P2P is supported on the device or not using
    package manager.
    b) Create a separate target side test for P2P specific tests.
    
    Note:
    1. Do NOT add binary-test-disable-framework in AndroidTest.xml for host side tests, it will cause the shell command "pm list features" failed.
    This is also the way to verify VTS tests if WIFI-Direct feature is not supported.
    2. Add precondition-feature for P2P specific tests. If P2P features is not supported, all tests will be skipped.
    
    Bug: 73306751
    Test: Ran VTS tests
    vts-tradefed run commandAndExit vts-hal --skip-all-system-status-check --primary-abi-only --skip-preconditions --module VtsHalWifiSupplicantV1_0Host -l INFO
    vts-tradefed run commandAndExit vts-hal --skip-all-system-status-check --primary-abi-only --skip-preconditions --module VtsHalWifiSupplicantV1_1Host -l INFO
    vts-tradefed run commandAndExit vts-hal --skip-all-system-status-check --primary-abi-only --skip-preconditions --module VtsHalWifiSupplicantP2pV1_0Target -l INFO
    
    Change-Id: I65de3ff5cde023bfba2ac3a6c38ff9ad680de8bc

--- wifi/supplicant/V1_0/target/p2p/Android.mk
+++ wifi/supplicant/V1_0/target/p2p/Android.mk
@@ -18,5 +18,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := VtsHalWifiSupplicantV1_1Target
+LOCAL_MODULE := VtsHalWifiSupplicantP2pV1_0Target
+VTS_CONFIG_SRC_DIR := testcases/hal/wifi/supplicant/V1_0/target/p2p
 include test/vts/tools/build/Android.host_config.mk
