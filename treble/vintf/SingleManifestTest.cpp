msft-mirror-aosp/platform.test.vts-testcase.hal

commit cc24ca47869880b25f33288469cc2ea3674bbaf7

Treble VINTF test: parameterize on HAL instances.
Before the change, some tests use a for loop to
iteratively query each HAL for information. When
a subset of the HAL fails, the whole test fails.

This change parameterize these tests on the
HAL name so each HAL instance has its own test case.
This allows faster triaging and declared
clearer ownership when handling test failures.

Note: As a side-effect of this change, time outs are only
imposed on certain binder transactions (e.g.
getting a HAL service), but no longer
on the whole test case of a HAL to avoid over complicating
the test. Hence,
it is technically possible that the operations after
getting the HAL service (e.g. getting hash chain,
getting debug info, etc.) are stuck. However, the usual
case is that a HAL is not responsive before the test,
which will hit the timeout at getting the HAL service
as the first step. In addition, even without this change,
when the whole test case times out, a warning message
is printed but no error is raised.

Bug: 240314930
Test: vts_treble_vintf_test_all
Test: atest vts_treble_vintf_vendor_test
Test: atest vts_treble_vintf_framework_test

Change-Id: I657b86cfcc266a83c10dbfd90692eb7286cc5271

treble/vintf/SingleManifestTest.h
@@ -23,12 +23,35 @@ namespace android {
 namespace vintf {
 namespace testing {
 
-// A parameterized test for common tests on device and framework manifest.
-class SingleManifestTest
-    : public VtsTrebleVintfTestBase,
-      public ::testing::WithParamInterface<HalManifestPtr> {
+// A parameterized test for an HIDL HAL declared in a device or framework
+// manifest.
+class SingleHidlTest : public VtsTrebleVintfTestBase,
+                       public ::testing::WithParamInterface<
+                           std::tuple<HidlInstance, HalManifestPtr>> {
  public:
-  virtual ~SingleManifestTest() {}
+  virtual ~SingleHidlTest() {}
+};
+
+// A parameterized test for an HIDL HAL registered through hwservicemanager
+// for a given device or framework manifest.
+class SingleHwbinderHalTest
+    : public VtsTrebleVintfTestBase,
+      public ::testing::WithParamInterface<
+          std::tuple<std::string /* fq instance name */, HalManifestPtr>> {
+ public:
+  virtual ~SingleHwbinderHalTest() {}
+
+  static std::string GetTestCaseSuffix(
+      const ::testing::TestParamInfo<ParamType>& info);
+};
+
+// A parameterized test for an AIDL HAL declared in a device or framework
+// manifest.
+class SingleAidlTest : public VtsTrebleVintfTestBase,
+                       public ::testing::WithParamInterface<
+                           std::tuple<AidlInstance, HalManifestPtr>> {
+ public:
+  virtual ~SingleAidlTest() {}
 };
 
 }  // namespace testing

treble/vintf/DeviceManifestTest.cpp
@@ -23,6 +23,10 @@
 
 #include "SingleManifestTest.h"
 
+using testing::Combine;
+using testing::Values;
+using testing::ValuesIn;
+
 namespace android {
 namespace vintf {
 namespace testing {
@@ -171,14 +175,25 @@ TEST_F(DeviceManifestTest, GrallocHalVersionCompatibility) {
       "android.hardware.graphics.allocator", {3, 0}, "IAllocator", "default"));
 }
 
-static std::vector<HalManifestPtr> GetTestManifests() {
-  return {
-      VintfObject::GetDeviceHalManifest(),
-  };
-}
+INSTANTIATE_TEST_CASE_P(
+    DeviceManifest, SingleHidlTest,
+    Combine(ValuesIn(VtsTrebleVintfTestBase::GetHidlInstances(
+                VintfObject::GetDeviceHalManifest())),
+            Values(VintfObject::GetDeviceHalManifest())),
+    &GetTestCaseSuffix<SingleHidlTest>);
 
-INSTANTIATE_TEST_CASE_P(DeviceManifest, SingleManifestTest,
-                        ::testing::ValuesIn(GetTestManifests()));
+INSTANTIATE_TEST_CASE_P(
+    DeviceManifest, SingleHwbinderHalTest,
+    Combine(ValuesIn(SingleHwbinderHalTest::ListRegisteredHwbinderHals()),
+            Values(VintfObject::GetDeviceHalManifest())),
+    &SingleHwbinderHalTest::GetTestCaseSuffix);
+
+INSTANTIATE_TEST_CASE_P(
+    DeviceManifest, SingleAidlTest,
+    Combine(ValuesIn(VtsTrebleVintfTestBase::GetAidlInstances(
+                VintfObject::GetDeviceHalManifest())),
+            Values(VintfObject::GetDeviceHalManifest())),
+    &GetTestCaseSuffix<SingleAidlTest>);
 
 }  // namespace testing
 }  // namespace vintf

treble/vintf/SystemVendorTest.cpp
@@ -20,7 +20,9 @@
 
 #include <android-base/logging.h>
 #include <android-base/strings.h>
+#include <gmock/gmock.h>
 #include <vintf/VintfObject.h>
+
 #include <iostream>
 
 #include "SingleManifestTest.h"
@@ -30,6 +32,10 @@ namespace vintf {
 namespace testing {
 
 using std::endl;
+using ::testing::Combine;
+using ::testing::Contains;
+using ::testing::Values;
+using ::testing::ValuesIn;
 
 // Tests that device manifest and framework compatibility matrix are compatible.
 TEST_F(SystemVendorTest, DeviceManifestFrameworkMatrixCompatibility) {
@@ -117,40 +123,65 @@ static void insert(D *d, const S &s) {
   d->insert(s.begin(), s.end());
 }
 
-// This needs to be tested besides
-// SingleManifestTest.ServedHwbinderHalsAreInManifest because some HALs may
-// refuse to provide its PID, and the partition cannot be inferred.
-TEST_F(SystemVendorTest, ServedHwbinderHalsAreInManifest) {
+std::set<std::string>
+    SystemVendorSingleHwbinderHalTest::manifest_hwbinder_hals_;
+void SystemVendorSingleHwbinderHalTest::SetUpTestSuite() {
   auto device_manifest = VintfObject::GetDeviceHalManifest();
   ASSERT_NE(device_manifest, nullptr) << "Failed to get device HAL manifest.";
   auto fwk_manifest = VintfObject::GetFrameworkHalManifest();
   ASSERT_NE(fwk_manifest, nullptr) << "Failed to get framework HAL manifest.";
 
-  std::set<std::string> manifest_hwbinder_hals;
-
-  insert(&manifest_hwbinder_hals, GetHwbinderHals(fwk_manifest));
-  insert(&manifest_hwbinder_hals, GetHwbinderHals(device_manifest));
-
-  Return<void> ret = default_manager_->list([&](const auto &list) {
-    for (const auto &name : list) {
-      // TODO(b/73774955): use standardized parsing code for fqinstancename
-      if (std::string(name).find(IBase::descriptor) == 0) continue;
-
-      EXPECT_NE(manifest_hwbinder_hals.find(name), manifest_hwbinder_hals.end())
-          << name << " is being served, but it is not in a manifest.";
-    }
-  });
-  EXPECT_TRUE(ret.isOk());
+  insert(&manifest_hwbinder_hals_,
+         GetDeclaredHidlHalsOfTransport(fwk_manifest, Transport::HWBINDER));
+  insert(&manifest_hwbinder_hals_,
+         GetDeclaredHidlHalsOfTransport(device_manifest, Transport::HWBINDER));
 }
 
-static std::vector<HalManifestPtr> GetTestManifests() {
-  return {
-      VintfObject::GetFrameworkHalManifest(),
-  };
+std::string SystemVendorSingleHwbinderHalTest::GetTestCaseSuffix(
+    const ::testing::TestParamInfo<ParamType> &info) {
+  return SanitizeTestCaseName(info.param) + "_" +
+         std::to_string(info.index);
 }
 
-INSTANTIATE_TEST_CASE_P(FrameworkManifest, SingleManifestTest,
-                        ::testing::ValuesIn(GetTestManifests()));
+// This needs to be tested besides
+// SingleManifestTest.ServedHwbinderHalIsInManifest because some HALs may
+// refuse to provide its PID, and the partition cannot be inferred.
+TEST_P(SystemVendorSingleHwbinderHalTest, ServedHwbinderHalIsInManifests) {
+  const auto &fq_instance_name = GetParam();
+  if (fq_instance_name.find(IBase::descriptor) == 0) {
+    GTEST_SKIP() << "Skipping for IBase: " << fq_instance_name;
+  }
+
+  EXPECT_THAT(manifest_hwbinder_hals_, Contains(fq_instance_name))
+      << fq_instance_name << " is being served, but it is not in a manifest.";
+}
+
+INSTANTIATE_TEST_CASE_P(
+    SystemVendorTest, SystemVendorSingleHwbinderHalTest,
+    ValuesIn(SingleHwbinderHalTest::ListRegisteredHwbinderHals()),
+    &SystemVendorSingleHwbinderHalTest::GetTestCaseSuffix);
+
+// Test framework manifest only
+
+INSTANTIATE_TEST_CASE_P(
+    FrameworkManifest, SingleHidlTest,
+    Combine(ValuesIn(VtsTrebleVintfTestBase::GetHidlInstances(
+                VintfObject::GetFrameworkHalManifest())),
+            Values(VintfObject::GetFrameworkHalManifest())),
+    &GetTestCaseSuffix<SingleHidlTest>);
+
+INSTANTIATE_TEST_CASE_P(
+    FrameworkManifest, SingleHwbinderHalTest,
+    Combine(ValuesIn(SingleHwbinderHalTest::ListRegisteredHwbinderHals()),
+            Values(VintfObject::GetFrameworkHalManifest())),
+    &SingleHwbinderHalTest::GetTestCaseSuffix);
+
+INSTANTIATE_TEST_CASE_P(
+    FrameworkManifest, SingleAidlTest,
+    Combine(ValuesIn(VtsTrebleVintfTestBase::GetAidlInstances(
+                VintfObject::GetFrameworkHalManifest())),
+            Values(VintfObject::GetFrameworkHalManifest())),
+    &GetTestCaseSuffix<SingleAidlTest>);
 
 }  // namespace testing
 }  // namespace vintf

treble/vintf/SystemVendorTest.h
@@ -31,6 +31,20 @@ class SystemVendorTest : public VtsTrebleVintfTestBase {
   virtual ~SystemVendorTest(){};
 };
 
+// A parameterized test for an HIDL HAL registered through hwservicemanager.
+class SystemVendorSingleHwbinderHalTest
+    : public SystemVendorTest,
+      public ::testing::WithParamInterface<std::string /* fq instance name */> {
+ public:
+  static void SetUpTestSuite();
+  virtual ~SystemVendorSingleHwbinderHalTest() {}
+  static std::string GetTestCaseSuffix(
+      const ::testing::TestParamInfo<ParamType>& info);
+
+ protected:
+  static std::set<std::string> manifest_hwbinder_hals_;
+};
+
 }  // namespace testing
 }  // namespace vintf
 }  // namespace android

treble/vintf/VtsTrebleVintfTestBase.cpp
@@ -21,6 +21,7 @@
 #include <android-base/strings.h>
 #include <android/hidl/manager/1.0/IServiceManager.h>
 #include <binder/IServiceManager.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <hidl-hash/Hash.h>
 #include <hidl-util/FQName.h>
@@ -80,62 +81,39 @@ using std::set;
 using std::string;
 using std::vector;
 
-void VtsTrebleVintfTestBase::SetUp() {
-  default_manager_ = ::android::hardware::defaultServiceManager();
-  ASSERT_NE(default_manager_, nullptr)
-      << "Failed to get default service manager." << endl;
+using ::testing::AnyOf;
+using ::testing::Eq;
+
+sp<IServiceManager> VtsTrebleVintfTestBase::default_manager() {
+  static auto default_manager = ::android::hardware::defaultServiceManager();
+  if (default_manager == nullptr) {
+    ADD_FAILURE() << "Failed to get default service manager.";
+  }
+  return default_manager;
 }
 
-void VtsTrebleVintfTestBase::ForEachHidlHalInstance(
-    const HalManifestPtr &manifest, HidlVerifyFn fn) {
-  manifest->forEachInstance([manifest, fn](const auto &manifest_instance) {
-    if (manifest_instance.format() != HalFormat::HIDL) {
-      return true;  // continue to next instance
-    }
-    const FQName fq_name{manifest_instance.package(),
-                         to_string(manifest_instance.version()),
-                         manifest_instance.interface()};
-    const Transport transport = manifest_instance.transport();
-    const std::string instance_name = manifest_instance.instance();
-
-    auto future_result =
-        std::async([&]() { fn(fq_name, instance_name, transport); });
-    int timeout_multiplier = base::GetIntProperty("ro.hw_timeout_multiplier", 1);
-    auto timeout = timeout_multiplier * std::chrono::seconds(1);
-    std::future_status status = future_result.wait_for(timeout);
-    if (status != std::future_status::ready) {
-      cout << "Timed out on: " << fq_name.string() << " " << instance_name
-           << endl;
+std::vector<HidlInstance> VtsTrebleVintfTestBase::GetHidlInstances(
+    const HalManifestPtr &manifest) {
+  std::vector<HidlInstance> ret;
+  manifest->forEachInstance([manifest, &ret](const auto &manifest_instance) {
+    if (manifest_instance.format() == HalFormat::HIDL) {
+      ret.emplace_back(manifest_instance);
     }
     return true;  // continue to next instance
   });
+  return ret;
 }
 
-void VtsTrebleVintfTestBase::ForEachAidlHalInstance(
-    const HalManifestPtr &manifest, AidlVerifyFn fn) {
-  manifest->forEachInstance([manifest, fn](const auto &manifest_instance) {
-    if (manifest_instance.format() != HalFormat::AIDL) {
-      return true;  // continue to next instance
-    }
-    const std::string &package = manifest_instance.package();
-    uint64_t version = manifest_instance.version().minorVer;
-    const std::string &interface = manifest_instance.interface();
-    const std::string &instance = manifest_instance.instance();
-    const std::optional<std::string> &updatable_via_apex =
-        manifest_instance.updatableViaApex();
-
-    auto future_result = std::async([&]() {
-      fn(package, version, interface, instance, updatable_via_apex);
-    });
-    int timeout_multiplier = base::GetIntProperty("ro.hw_timeout_multiplier", 1);
-    auto timeout = timeout_multiplier * std::chrono::seconds(1);
-    std::future_status status = future_result.wait_for(timeout);
-    if (status != std::future_status::ready) {
-      cout << "Timed out on: " << package << "." << interface << "/" << instance
-           << endl;
+std::vector<AidlInstance> VtsTrebleVintfTestBase::GetAidlInstances(
+    const HalManifestPtr &manifest) {
+  std::vector<AidlInstance> ret;
+  manifest->forEachInstance([manifest, &ret](const auto &manifest_instance) {
+    if (manifest_instance.format() == HalFormat::AIDL) {
+      ret.emplace_back(manifest_instance);
     }
     return true;  // continue to next instance
   });
+  return ret;
 }
 
 sp<IBase> VtsTrebleVintfTestBase::GetHidlService(const FQName &fq_name,
@@ -228,55 +206,38 @@ Partition VtsTrebleVintfTestBase::GetPartition(sp<IBase> hal_service) {
   return partition;
 }
 
-set<string> VtsTrebleVintfTestBase::GetPassthroughHals(
-    HalManifestPtr manifest) {
-  std::set<std::string> manifest_passthrough_hals_;
-
-  auto add_manifest_hals = [&manifest_passthrough_hals_](
-                               const FQName &fq_name,
-                               const string &instance_name,
-                               Transport transport) {
-    if (transport == Transport::HWBINDER) {
-      // ignore
-    } else if (transport == Transport::PASSTHROUGH) {
-      // 1.n in manifest => 1.0, 1.1, ... 1.n are all served (if they exist)
-      FQName fq = fq_name;
-      while (true) {
-        manifest_passthrough_hals_.insert(fq.string() + "/" + instance_name);
-        if (fq.getPackageMinorVersion() <= 0) break;
-        fq = fq.downRev();
-      }
-    } else {
-      ADD_FAILURE() << "Unrecognized transport: " << transport;
+set<string> VtsTrebleVintfTestBase::GetDeclaredHidlHalsOfTransport(
+    HalManifestPtr manifest, Transport transport) {
+  EXPECT_THAT(transport,
+              AnyOf(Eq(Transport::HWBINDER), Eq(Transport::PASSTHROUGH)))
+      << "Unrecognized transport of HIDL: " << transport;
+  std::set<std::string> ret;
+  for (const auto &hidl_instance : GetHidlInstances(manifest)) {
+    if (hidl_instance.transport() != transport) {
+      continue;  // ignore
     }
-  };
-  ForEachHidlHalInstance(manifest, add_manifest_hals);
-  return manifest_passthrough_hals_;
+
+    // 1.n in manifest => 1.0, 1.1, ... 1.n are all served (if they exist)
+    FQName fq = hidl_instance.fq_name();
+    while (true) {
+      ret.insert(fq.string() + "/" + hidl_instance.instance_name());
+      if (fq.getPackageMinorVersion() <= 0) break;
+      fq = fq.downRev();
+    }
+  }
+  return ret;
 }
 
-set<string> VtsTrebleVintfTestBase::GetHwbinderHals(HalManifestPtr manifest) {
-  std::set<std::string> manifest_hwbinder_hals_;
-
-  auto add_manifest_hals = [&manifest_hwbinder_hals_](
-                               const FQName &fq_name,
-                               const string &instance_name,
-                               Transport transport) {
-    if (transport == Transport::HWBINDER) {
-      // 1.n in manifest => 1.0, 1.1, ... 1.n are all served (if they exist)
-      FQName fq = fq_name;
-      while (true) {
-        manifest_hwbinder_hals_.insert(fq.string() + "/" + instance_name);
-        if (fq.getPackageMinorVersion() <= 0) break;
-        fq = fq.downRev();
-      }
-    } else if (transport == Transport::PASSTHROUGH) {
-      // ignore
-    } else {
-      ADD_FAILURE() << "Unrecognized transport: " << transport;
-    }
-  };
-  ForEachHidlHalInstance(manifest, add_manifest_hals);
-  return manifest_hwbinder_hals_;
+std::vector<std::string> VtsTrebleVintfTestBase::ListRegisteredHwbinderHals() {
+  std::vector<std::string> return_value;
+  EXPECT_NE(default_manager(), nullptr);
+  if (default_manager() == nullptr) return {};
+  Return<void> ret = default_manager()->list([&](const auto &list) {
+    return_value.reserve(list.size());
+    for (const auto &s : list) return_value.push_back(s);
+  });
+  EXPECT_TRUE(ret.isOk());
+  return return_value;
 }
 
 }  // namespace testing

treble/vintf/VtsTrebleVintfTestBase.h
@@ -39,11 +39,10 @@ using android::hidl::manager::V1_0::IServiceManager;
 class VtsTrebleVintfTestBase : public ::testing::Test {
  public:
   virtual ~VtsTrebleVintfTestBase() {}
-  virtual void SetUp() override;
 
   // Applies given function to each HAL instance in VINTF.
-  static void ForEachAidlHalInstance(const HalManifestPtr &, AidlVerifyFn);
-  static void ForEachHidlHalInstance(const HalManifestPtr &, HidlVerifyFn);
+  static std::vector<AidlInstance> GetAidlInstances(const HalManifestPtr &);
+  static std::vector<HidlInstance> GetHidlInstances(const HalManifestPtr &);
 
   // Retrieves an existing HAL service.
   static sp<IBase> GetHidlService(const string &fq_name,
@@ -59,12 +58,16 @@ class VtsTrebleVintfTestBase : public ::testing::Test {
 
   static vector<string> GetInterfaceChain(const sp<IBase> &service);
 
-  static set<string> GetPassthroughHals(HalManifestPtr manifest);
-  static set<string> GetHwbinderHals(HalManifestPtr manifest);
+  static set<string> GetDeclaredHidlHalsOfTransport(HalManifestPtr manifest,
+                                                Transport transport);
+
   Partition GetPartition(sp<IBase> hal_service);
 
-  // Default service manager.
-  sp<IServiceManager> default_manager_;
+  static sp<IServiceManager> default_manager();
+
+  // List HALs registered through hwservicemanager. Return the list of FQ
+  // instance names.
+  static std::vector<std::string> ListRegisteredHwbinderHals();
 };
 
 }  // namespace testing

treble/vintf/utils.cpp
@@ -17,12 +17,13 @@
 
 #include "utils.h"
 
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+
 #include <map>
 #include <set>
 #include <string>
 #include <vector>
-
-#include <android-base/properties.h>
 
 using android::base::GetUintProperty;
 
@@ -49,6 +50,49 @@ const set<string> kPassthroughHals = {
     "android.hardware.graphics.mapper", "android.hardware.renderscript",
     "android.hidl.memory",
 };
+
+std::string SanitizeTestCaseName(std::string original) {
+  for (char &c : original) {
+    if (!isalnum(c)) {
+      c = '_';
+    }
+  }
+  return original;
+}
+
+HidlInstance::HidlInstance(const ManifestInstance &other)
+    : ManifestInstance(other) {
+  CHECK_EQ(format(), HalFormat::HIDL);
+}
+
+ostream &operator<<(ostream &os, const HidlInstance& val) {
+  return os << val.transport() << " HAL " << val.fq_name().string() << "/"
+            << val.instance_name();
+}
+
+string HidlInstance::test_case_name() const {
+  return SanitizeTestCaseName(fq_name().string() + "/" + instance_name());
+}
+
+AidlInstance::AidlInstance(const ManifestInstance &other)
+    : ManifestInstance(other) {
+  CHECK_EQ(format(), HalFormat::AIDL);
+}
+
+ostream &operator<<(ostream &os, const AidlInstance& val) {
+  os << toAidlFqnameString(val.package(), val.interface(), val.instance())
+     << ", Version " << val.version();
+  if (val.updatable_via_apex()) {
+    os << ", updatable_via_apex = " << *val.updatable_via_apex();
+  }
+  return os;
+}
+
+string AidlInstance::test_case_name() const {
+  return SanitizeTestCaseName(
+      toAidlFqnameString(package(), interface(), instance()) + "_V" +
+      to_string(version()));
+}
 
 uint64_t GetBoardApiLevel() {
   return GetUintProperty<uint64_t>("ro.vendor.api_level", 0);

commit 2bec0220cf4502ab9c3bf21b977389a0aa96efcf

Treble VINTF test: Allow vendor extensions for passthrough HALs.
The change cc24ca47869880b25f33288469cc2ea3674bbaf7
"Treble VINTF test: parameterize on HAL instances."

  https://android.googlesource.com/platform/test/vts-testcase/hal/+/cc24ca47869880b25f33288469cc2ea3674bbaf7

accidentally introduced a requirement on passhthrough
vendor extension HALs that the dynamic library (.so)
with the name of the vendor extension must exist. For example,
if vendor.foo.display.mapper extends android.hardware.graphics.mapper,
it was erroneously required that vendor.foo.display.mapper@3.0-impl.so
exists. This was because when the test cannot find
vendor.foo.display.mapper@3.0-impl.so, it skips calling interfaceChain
on it, thus it cannot infer that it extends a HAL that is in
the passthrough allowlist.

Remove this incorrectly imposed requirement by actually searching for
all passthrough HALs on the device and look for the implementation
that also implements vendor.foo.display.mapper@3.0. Then, look up
intefaceChain() on the implementation to match against the allowlist.

Bug: 261765512
Test: vts_treble_vintf_vendor_test --gtest_filter=*vendor* on a pixel
  device

Change-Id: I06bc822e3af82ad5c7ca53207a10175610779b51

treble/vintf/SingleManifestTest.cpp
@@ -106,7 +106,8 @@ static FqInstance ToFqInstance(const string &interface,
 // Given android.foo.bar@x.y::IFoo/default, attempt to get
 // android.foo.bar@x.y::IFoo/default, android.foo.bar@x.(y-1)::IFoo/default,
 // ... android.foo.bar@x.0::IFoo/default until the passthrough HAL is retrieved.
-static sp<IBase> GetPassthroughService(const FqInstance &fq_instance) {
+static sp<IBase> GetPassthroughServiceExact(const FqInstance &fq_instance,
+                                            bool expect_interface_chain_valid) {
   for (size_t minor_version = fq_instance.getMinorVersion();; --minor_version) {
     // String out instance name from fq_instance.
     FqInstance interface;
@@ -131,7 +132,7 @@ static sp<IBase> GetPassthroughService(const FqInstance &fq_instance) {
           }
         }
       });
-      if (!interface_chain_valid) {
+      if (!interface_chain_valid && expect_interface_chain_valid) {
         ADD_FAILURE() << "Retrieved " << interface.string() << "/"
                       << fq_instance.getInstance() << " as "
                       << fq_instance.string()
@@ -150,6 +151,82 @@ static sp<IBase> GetPassthroughService(const FqInstance &fq_instance) {
     }
   }
   ADD_FAILURE() << "Should not reach here";
+  return nullptr;
+}
+
+// Given vendor.foo.bar@x.y::IFoo/default, also look up all declared passthrough
+// HAL implementations on the device that implements this interface.
+sp<IBase> SingleHidlTest::GetPassthroughService(const FqInstance &fq_instance) {
+  sp<IBase> hal_service = GetPassthroughServiceExact(
+      fq_instance, true /* expect_interface_chain_valid */);
+  if (hal_service != nullptr) {
+    return hal_service;
+  }
+
+  // For vendor extensions, hal_service may be null because we don't know
+  // its interfaceChain()[1] to call getService(). However, the base interface
+  // should be declared in the manifest. Attempt to find it.
+  cout
+      << "Can't find passthrough service " << fq_instance.string()
+      << ". It might be a vendor extension. Searching all passthrough services "
+         "on the device for a match."
+      << endl;
+
+  const auto &[_, manifest] = GetParam();
+  auto all_declared_passthrough_instances = GetHidlInstances(manifest);
+  for (const HidlInstance &other_hidl_instance :
+       all_declared_passthrough_instances) {
+    if (other_hidl_instance.transport() != Transport::PASSTHROUGH) {
+      continue;
+    }
+    if (other_hidl_instance.instance_name() != fq_instance.getInstance()) {
+      cout << "Skipping " << other_hidl_instance.fq_name().string() << "/"
+           << other_hidl_instance.instance_name()
+           << " because instance name is not " << fq_instance.getInstance();
+      continue;
+    }
+    auto other_fq_instance = FqInstance::from(
+        other_hidl_instance.fq_name(), other_hidl_instance.instance_name());
+    if (!other_fq_instance) {
+      cout << other_hidl_instance.fq_name().string() << "/"
+           << other_hidl_instance.instance_name()
+           << " is not a valid FqInstance, skipping." << endl;
+      continue;
+    }
+    auto other_service = GetPassthroughServiceExact(
+        *other_fq_instance, false /* expect_interface_chain_valid */);
+    if (other_service == nullptr) {
+      cout << "Cannot retrieve " << other_fq_instance->string() << ", skipping."
+           << endl;
+      continue;
+    }
+    bool match = false;
+    auto other_interface_chain_ret =
+        other_service->interfaceChain([&](const auto &chain) {
+          for (const auto &intf : chain) {
+            auto other_fq_instance_in_chain = FqInstance::from(
+                std::string(intf) + "/" + other_fq_instance->getInstance());
+            if (other_fq_instance_in_chain == fq_instance) {
+              match = true;
+              break;
+            }
+          }
+        });
+    if (!other_interface_chain_ret.isOk()) {
+      cout << "Cannot call interfaceChain on " << other_fq_instance->string()
+           << ", skipping." << endl;
+      continue;
+    }
+    if (match) {
+      cout << "The implementation of " << other_fq_instance->string()
+           << " also implements " << fq_instance.string()
+           << ", using it to check if passthrough is allowed for "
+           << fq_instance.string() << endl;
+      return other_service;
+    }
+  }
+  cout << "Can't find any other passthrough service implementing "
+       << fq_instance.string() << endl;
   return nullptr;
 }
 

treble/vintf/SingleManifestTest.h
@@ -17,6 +17,8 @@
 #ifndef VTS_TREBLE_VINTF_TEST_SINGLE_MANIFEST_TEST_H_
 #define VTS_TREBLE_VINTF_TEST_SINGLE_MANIFEST_TEST_H_
 #include <gtest/gtest.h>
+#include <hidl-util/FqInstance.h>
+
 #include "VtsTrebleVintfTestBase.h"
 
 namespace android {
@@ -30,6 +32,8 @@ class SingleHidlTest : public VtsTrebleVintfTestBase,
                            std::tuple<HidlInstance, HalManifestPtr>> {
  public:
   virtual ~SingleHidlTest() {}
+
+  sp<IBase> GetPassthroughService(const android::FqInstance& fq_instance);
 };
 
 // A parameterized test for an HIDL HAL registered through hwservicemanager